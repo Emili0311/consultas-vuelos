94675591761b6b3310fa59a0d7b6fa516bba3218
Emili0311 <emiliyoselyn.gutierrezsalas.gen23@cbtis105.edu.mx>
2025-05-14 10:05:33 -0600

    avance2

--- Controllers/Api/VuelosController.cs
+++ Controllers/Api/VuelosController.cs
@@ -1,49 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 
 [Route("api/vuelos")]
-public class VuelosCotroller : ControllerBase {
-
+public class VuelosController : ControllerBase
+{
     [HttpGet("ciudades-origen")]
-    public IActionResult CiudadesOrigen(){
+    public IActionResult CiudadesOrigen()
+    {
         var client = new MongoClient(CadenaConexion.MONGO_DB);
         var db = client.GetDatabase("Aeropuerto");
         var collection = db.GetCollection<Vuelo>("Vuelos");
 
         var lista = collection.Distinct<string>("CiudadOrigen", FilterDefinition<Vuelo>.Empty).ToList();
+
         return Ok(lista);
-    
     }
 
     [HttpGet("ciudades-destino")]
-    public IActionResult CiudadesDestino(){
-        var client = new MongoClient(CadenaConexion.MONGO_DB);
+    public IActionResult CiudadesDestino()
+    {
+         var client = new MongoClient(CadenaConexion.MONGO_DB);
         var db = client.GetDatabase("Aeropuerto");
         var collection = db.GetCollection<Vuelo>("Vuelos");
 
         var lista = collection.Distinct<string>("CiudadDestino", FilterDefinition<Vuelo>.Empty).ToList();
+
         return Ok(lista);
-    
     }
 
     [HttpGet("estatus")]
-    public IActionResult ListarEstatus(){
+    public IActionResult ListarEstatus()
+    {
         var client = new MongoClient(CadenaConexion.MONGO_DB);
         var db = client.GetDatabase("Aeropuerto");
         var collection = db.GetCollection<Vuelo>("Vuelos");
 
         var lista = collection.Distinct<string>("EstatusVuelo", FilterDefinition<Vuelo>.Empty).ToList();
+
         return Ok(lista);
-    
     }
 
     [HttpGet("listar-vuelos")]
-    public IActionResult ListarVuelos(){
-         var client = new MongoClient(CadenaConexion.MONGO_DB);
-        var db = client.GetDatabase("Aeropuerto");
-        var collection = db.GetCollection<Vuelo>("Vuelos");
-
-        var lista = collection.Distinct<string>("ListarVuelo", FilterDefinition<Vuelo>.Empty).ToList();
+    public IActionResult ListarVuelos()
+    {
+        var lista = _vuelos.Find(FilterDefinition<Vuelo>.Empty).ToList();
         return Ok(lista);
-    
     }
 }
